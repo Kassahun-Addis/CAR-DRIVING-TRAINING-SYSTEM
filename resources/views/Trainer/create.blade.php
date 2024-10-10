e77df7c7a1a2d411c93b11cf1ac11bd5ea1eefa1
Kassahun Addiss <kassahunaddiss6@gmail.com>
2024-10-10 17:33:57 +0300

    some functionalities add

--- resources/views/Trainer/create.blade.php
+++ resources/views/Trainer/create.blade.php
@@ -13,9 +13,11 @@
             </ul>
         </div>
     @endif
-
+    <div class="form-section">
     <form action="{{ route('trainers.store') }}" method="POST">
         @csrf
+        <div class="row">
+        <div class="col-12">
 
         <div class="form-group">
             <label for="name">Name:</label>
@@ -50,8 +52,18 @@
                 @endforeach
             </select>
         </div>
+        </div>
+</div>
 
-        <button type="submit" class="btn btn-primary">Register Trainer</button>
+
+<div class="d-flex justify-content-center">
+                <button type="submit" class="btn btn-primary btn-custom">Save</button>
+                <button type="reset" class="btn btn-secondary btn-custom">Reset</button>
+                <a href="{{ route('trainers.index') }}" class="btn btn-secondary btn-custom">Back to list</a>
+        </div>
+    
     </form>
+    </div>
+
 </div>
 @endsection
